9c3727264cc494e02c873b7eb8242d02237ea1b9
RAM PRADEEP  R P <rampradeep4858@gmail.com>
2026-08-02 18:38:55 +0530

    Add backend/src/main/java/com/vsbec/feedback/repository/AcademicYearRepository.java

---
+++ backend/src/main/java/com/vsbec/feedback/repository/AcademicYearRepository.java
@@ -0,0 +1,7 @@
+package com.vsbec.feedback.repository;
+
+import com.vsbec.feedback.entity.AcademicYear;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AcademicYearRepository extends JpaRepository<AcademicYear, Long> {
+}
